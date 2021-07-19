9ec41a707b1c5e3a87c0b1cca5617ade160c3c25
zpskek <wjdghdwns0@gmail.com>
2021-07-19 14:59:14 +0900

    Create seed_movies

---
+++ movies/management/commands/seed_movies.py
@@ -0,0 +1,56 @@
+import random
+from typing import Any, Optional
+
+from django.core.management.base import BaseCommand, CommandParser
+from django.db.models.query_utils import Q
+from django.contrib.admin.utils import flatten
+
+from django_seed import Seed
+
+from movies.models import Movie
+from categories.models import Category
+from people.models import Person
+
+NAME = "movies"
+
+
+class Command(BaseCommand):
+    help = f"This command creates many {NAME}"
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "--total", type=int, help=f"How many {NAME} do you want to create"
+        )
+
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        total = options.get("total", 1)
+        directors = Person.objects.all().filter(kind="director")
+        seeder = Seed.seeder()
+        seeder.add_entity(
+            Movie,
+            total,
+            {
+                "year": lambda x: random.randint(1900, 2021),
+                "rating": lambda x: random.randint(0, 5),
+                "director": lambda x: random.choice(directors),
+            },
+        )
+        created_movies = seeder.execute()
+        movie_pks = flatten(created_movies.values())
+
+        categories = Category.objects.all().filter(Q(kind="both") | Q(kind="movie"))
+        casts = Person.objects.all().filter(kind="actor")
+
+        for movie_pk in movie_pks:
+            movie = Movie.objects.get(pk=movie_pk)
+            for category in categories:
+                magic_number = random.randint(0, 21)
+                if magic_number % 2 == 0:
+                    movie.categories.add(category)
+
+            for cast in casts:
+                magic_number = random.randint(0, 16)
+                if magic_number % 2 == 0:
+                    movie.casts.add(cast)
+
+        self.stdout.write(self.style.SUCCESS(f"Create {NAME}"))
