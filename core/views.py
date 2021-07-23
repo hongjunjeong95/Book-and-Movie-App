68e0a17c5923a4c50284da8ee69696d06ed2215f
zpskek <wjdghdwns0@gmail.com>
2021-07-23 16:06:48 +0900

    Remove HomeView on core views.py

--- core/views.py
+++ core/views.py
@@ -1,30 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
 from django.core.paginator import Paginator
 
 from books.models import Book
 from movies.models import Movie
 from people.models import Person
-
-
-class HomeView(ListView):
-
-    """Home View Definition"""
-
-    model = Book
-    template_name = "pages/root/home.html"
-    context_object_name = "books"
-    paginate_by = 12
-    paginate_orphans = 6
-    ordering = "created"
-
-    def get_context_data(self):
-        page = int(self.request.GET.get("page", 1))
-        page_sector = (page - 1) // 5
-        page_sector = page_sector * 5
-        context = super().get_context_data()
-        context["page_sector"] = page_sector
-        return context
 
 
 def homeView(request):
