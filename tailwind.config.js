3d05b2fd2ea8242c1e5b7559525578accc729139
zpskek <wjdghdwns0@gmail.com>
2021-07-27 15:57:10 +0900

    Setup tailwind with scss using gulp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      inset: {
+        22: "5.5rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
