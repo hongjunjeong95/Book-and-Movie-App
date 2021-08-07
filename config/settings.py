eb380aa23a7090061eae948ea5f7fb72cb0bead7
zpskek <wjdghdwns0@gmail.com>
2021-08-07 14:41:38 +0900

    Add dotenv

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ PROJECT_APPS = [
     "core.apps.CoreConfig",
 ]
 
-THIRD_PARTY_APPS = ["django_seed"]
+THIRD_PARTY_APPS = ["django_seed", "dotenv"]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
